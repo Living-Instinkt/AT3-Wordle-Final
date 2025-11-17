6c64d69c429696dd7413d418a10ee7ab96ac41c6
Living-instinkt <joshplank@outlook.com>
2025-11-17 13:24:14 +0800

    Init Commit

---
+++ wordle.py
@@ -0,0 +1,35 @@
+# JPL_ICTPRG302_Project_2025_S2
+#
+# Author: Josh Plank
+# Student ID: 20154551
+#
+# Course Cert IV: Programming
+# Lecturer: Para O'Kelly
+
+# TODO: Add Import statements (if needed)
+
+# Variables and Constants
+# TODO: Define Constants
+
+# TODO: Define Variables
+
+# Application Functions
+# TODO: Score Guess Function
+
+# TODO: Read File Into Word List Function
+
+# TODO: Display Greeting Function
+def show_greeting():
+    print("Welcome")
+
+# TODO: Display Instructions Function
+def show_instructions():
+    print("Instructions")
+
+# TODO: Any Optional Additional Functions
+
+# TODO: Play Game Function
+
+#TODO: Testing Function
+
+#TODO: Main Program
